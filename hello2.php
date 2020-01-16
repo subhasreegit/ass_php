ed5009531349b2d79f0b123bee3ac0b62453183a
subhasreegit <58703100+subhasreegit@users.noreply.github.com>
2020-01-16 17:29:52 +0530

    Update hello2.php

--- hello2.php
+++ hello2.php
@@ -5,15 +5,16 @@
 </head>
 <body>
 	<?php
-		if(isset($_POST['upload'])){
+		if(isset($_POST['upload'])){//we are checking the variable is set or not
 			/*$file= $_FILES['file'];
 			print_r($file);*/
-
+			//here we are checking and storing the detail about the uploaded file.
 			$file_name=$_FILES['file']['name'];
 			$file_type=$_FILES['file']['type'];
 			$file_size=$_FILES['file']['size'];
 			$file_tem_loc=$_FILES['file']['tmp_name'];
 			$file_store="images/".$file_name;
+			//Through this uploaded file is stored in the folder and displayed through the concatination in the field of echo
 			if(move_uploaded_file($file_tem_loc, $file_store)){
 				echo "<img src=".$file_store." width='150px' height='150px'>";
 			}		
