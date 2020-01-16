93884c601e4c5d63300353d9a8c615186412f289
subhasreegit <58703100+subhasreegit@users.noreply.github.com>
2020-01-16 17:30:25 +0530

    Update hello.php

--- hello.php
+++ hello.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 	Hello <?php
-		echo $_POST["artistfield_fname"]," " , $_POST["artistfield_lname"];<!-- Through the post method we have taken the data from the other page and and displayed the data in the new page through echo method-->
+		echo $_POST["artistfield_fname"]," " , $_POST["artistfield_lname"];// Through the post method we have taken the data from the other page and and displayed the data in the new page through echo method
 	 ?>
 	
 </body>
