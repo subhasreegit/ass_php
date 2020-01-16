c1369081c14533379a13ef280d60862448fb657e
subhasreegit <58703100+subhasreegit@users.noreply.github.com>
2020-01-16 17:20:56 +0530

    Update assignment_1.php

--- assignment_1.php
+++ assignment_1.php
@@ -3,6 +3,7 @@
     <head>
         <title>My Page</title>
         <script src="//code.jquery.com/jquery-1.9.1.js"></script> 
+    <!-- Through this function we are trying to concatinae each word sumultaniously. at first i take the value as null. then as the user give the input the updated data is concatinated simultaniously.-->
     <script>
         function SetFullName()
         {
@@ -12,6 +13,7 @@
     </script>
 
     </head>
+    <!-- Through the input tag the input is taken nd it send to the next php page through the post method and in the action section of the from the address of the php file is given to go to the file when the user click on the submit buttonn.-->
     <body>
         <form action="http://localhost/hello.php" method="POST">
              <label>Firstname</label><br>
@@ -21,6 +23,7 @@
             <label>Full-Name</label><br>
             <input type="text" name="artistfield_fullname" id="Artist_fullname" Disabled required/><br><br>
             <input type="submit">
+            <!--through the pattern we are restricting the input inserted field. and through the submit button we are triggering the page to do the action and go to the next page. -->
         </form>
         
     </body>    
